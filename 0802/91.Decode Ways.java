aa9ebca48f2fb4347bea0fe7e2b73084bf783eb8
Emily FU <fumengyao5544@gmail.com>
2017-08-05 15:57:07 -0700

    Update 91.Decode Ways.java

--- 0802/91.Decode Ways.java
+++ 0802/91.Decode Ways.java
@@ -12,17 +12,20 @@
 
 // The number of ways decoding "12" is 2.
 
-  public int numDecodings(String s) {
+public class Solution {
+    public int numDecodings(String s) {
         if (s == null || s.length() == 0 || s.equals("0")) return 0;
         if (s.length() == 1 ) return 1;
         int[] dp = new int[s.length() + 1];
-    // dp[0] 不能设为0  ！！！
+       
+         // dp[0] 不能设为0  ！！！
         dp[0] = 1;
         if (s.charAt(0) == '0'){
             dp[1] = 0;
         }else {
             dp[1] = dp[0];
         }
+      
         for (int i = 2; i <= s.length(); i++) {
             if (s.charAt(i - 1) != '0') {
                 dp[i] += dp[i - 1];
@@ -34,3 +37,4 @@
         }
         return dp[s.length()];
     }
+}
