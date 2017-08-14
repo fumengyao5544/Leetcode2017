a644beccb69a07d731b2aeb62dab08bd489ce39c
Emily FU <fumengyao5544@gmail.com>
2017-08-13 18:06:13 -0700

    Update 121.Best Time to Buy and Sell Stock.java

--- 0813/121.Best Time to Buy and Sell Stock.java
+++ 0813/121.Best Time to Buy and Sell Stock.java
@@ -1,12 +1,2 @@
 
-public class Solution {
-    public boolean isValidBST(TreeNode root) {
-        if (root == null) return true;
-        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
-    }
-    public boolean helper (TreeNode node, Long min, Long max) {
-        if (node == null) return true;
-        if (node.val <= min || node.val >= max) return false;
-        return helper(node.left, min, (long)node.val) && helper(node.right, (long)node.val, max);
-    }
-}
+
