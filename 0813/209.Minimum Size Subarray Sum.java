7db18002e6204cf3e4d8add35ad6b5ad58910372
Emily FU <fumengyao5544@gmail.com>
2017-08-13 18:34:07 -0700

    Update 209.Minimum Size Subarray Sum.java

--- 0813/209.Minimum Size Subarray Sum.java
+++ 0813/209.Minimum Size Subarray Sum.java
@@ -1,1 +1,21 @@
 
+public class Solution {
+    public int minSubArrayLen(int s, int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int i = 0;
+        int j = 0;
+        int sum = 0;
+        int result = Integer.MAX_VALUE;
+        for (i = 0; i < nums.length; i++) {
+            while ( j < nums.length && sum < s) {
+                sum += nums[j];
+                j++;
+            }
+            if (sum >= s) {
+                result = Math.min(result, j - i);
+            }
+            sum -= nums[i];
+        }
+        return result == Integer.MAX_VALUE ? 0 : result;
+    }
+}
