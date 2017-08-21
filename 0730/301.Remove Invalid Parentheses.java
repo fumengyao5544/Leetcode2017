f29ee4bec8e646b2a41a521d83b470d7f0a8180c
Emily FU <fumengyao5544@gmail.com>
2017-08-20 17:07:50 -0700

    Update 301.Remove Invalid Parentheses.java

--- 0730/301.Remove Invalid Parentheses.java
+++ 0730/301.Remove Invalid Parentheses.java
@@ -1,3 +1,13 @@
+// Remove the minimum number of invalid parentheses in order to make the input string valid. Return all possible results.
+
+// Note: The input string may contain letters other than the parentheses ( and ).
+
+// Examples:
+// "()())()" -> ["()()()", "(())()"]
+// "(a)())()" -> ["(a)()()", "(a())()"]
+// ")(" -> [""]
+
+
 public class Solution {
     public List<String> removeInvalidParentheses(String s) {
          List<String> ans = new ArrayList<>();
