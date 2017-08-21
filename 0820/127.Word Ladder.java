a1ce77a551431f4d61e6dd8c28953b9086aafe93
Emily FU <fumengyao5544@gmail.com>
2017-08-20 17:24:08 -0700

    Update 127.Word Ladder.java

--- 0820/127.Word Ladder.java
+++ 0820/127.Word Ladder.java
@@ -1,22 +1,88 @@
-Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest transformation sequence from beginWord to endWord, such that:
+// Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest transformation sequence from beginWord to endWord, such that:
+// Only one letter can be changed at a time.
+// Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
+// For example,
+// Given:
+// beginWord = "hit"
+// endWord = "cog"
+// wordList = ["hot","dot","dog","lot","log","cog"]
+// As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+// return its length 5.
 
-Only one letter can be changed at a time.
-Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
-For example,
+// Note:
+// Return 0 if there is no such transformation sequence.
+// All words have the same length.
+// All words contain only lowercase alphabetic characters.
+// You may assume no duplicates in the word list.
+// You may assume beginWord and endWord are non-empty and are not the same.
+// UPDATE (2017/1/20):
+// The wordList parameter had been changed to a list of strings (instead of a set of strings). Please reload the code definition to get the latest changes.
 
-Given:
-beginWord = "hit"
-endWord = "cog"
-wordList = ["hot","dot","dog","lot","log","cog"]
-As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
-return its length 5.
 
-Note:
-Return 0 if there is no such transformation sequence.
-All words have the same length.
-All words contain only lowercase alphabetic characters.
-You may assume no duplicates in the word list.
-You may assume beginWord and endWord are non-empty and are not the same.
-UPDATE (2017/1/20):
-The wordList parameter had been changed to a list of strings (instead of a set of strings). Please reload the code definition to get the latest changes.
+  
+  public class Solution {
+    public int ladderLength(String start, String end, List<String> wordList) {
+        Set<String> dict = new HashSet<>();
+        for (String word : wordList) {
+            dict.add(word);
+        }
+        
+        if (start.equals(end)) {
+            return 1;
+        }
+        
+        HashSet<String> hash = new HashSet<String>();
+        Queue<String> queue = new LinkedList<String>();
+        queue.offer(start);
+        hash.add(start);
+        
+        int length = 1;
+        while (!queue.isEmpty()) {
+            length++;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String word = queue.poll();
+                for (String nextWord: getNextWords(word, dict)) {
+                    if (hash.contains(nextWord)) {
+                        continue;
+                    }
+                    if (nextWord.equals(end)) {
+                        return length;
+                    }
+                    
+                    hash.add(nextWord);
+                    queue.offer(nextWord);
+                }
+            }
+        }
+        
+        return 0;
+    }
 
+    // replace character of a string at given index to a given character
+    // return a new string
+    private String replace(String s, int index, char c) {
+        char[] chars = s.toCharArray();
+        chars[index] = c;
+        return new String(chars);
+    }
+    
+    // get connections with given word.
+    // for example, given word = 'hot', dict = {'hot', 'hit', 'hog'}
+    // it will return ['hit', 'hog']
+    private ArrayList<String> getNextWords(String word, Set<String> dict) {
+        ArrayList<String> nextWords = new ArrayList<String>();
+        for (char c = 'a'; c <= 'z'; c++) {
+            for (int i = 0; i < word.length(); i++) {
+                if (c == word.charAt(i)) {
+                    continue;
+                }
+                String nextWord = replace(word, i, c);
+                if (dict.contains(nextWord)) {
+                    nextWords.add(nextWord);
+                }
+            }
+        }
+        return nextWords;
+    }
+}
