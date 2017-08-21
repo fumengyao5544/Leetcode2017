c9ead037f9897aa15a7618579f834788019d9a14
Emily FU <fumengyao5544@gmail.com>
2017-08-20 17:14:34 -0700

    Update 273. Integer to English Words.java

--- 0730/273. Integer to English Words.java
+++ 0730/273. Integer to English Words.java
@@ -1,3 +1,10 @@
+// Convert a non-negative integer to its english words representation. Given input is guaranteed to be less than 231 - 1.
+// For example,
+// 123 -> "One Hundred Twenty Three"
+// 12345 -> "Twelve Thousand Three Hundred Forty Five"
+// 1234567 -> "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
+
+
 public class Solution {
     private final String[] LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
     private final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
