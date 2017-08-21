98f85d15a91d24112d29d53e6690d4b0abb7722b
Emily FU <fumengyao5544@gmail.com>
2017-08-20 17:09:08 -0700

    Update 67.Add Binary.java

--- 0730/67.Add Binary.java
+++ 0730/67.Add Binary.java
@@ -1,7 +1,35 @@
 
-Given two binary strings, return their sum (also a binary string).
+// Given two binary strings, return their sum (also a binary string).
 
-For example,
-a = "11"
-b = "1"
-Return "100".
+// For example,
+// a = "11"
+// b = "1"
+// Return "100".
+  
+  
+  
+  public class Solution {
+    public String addBinary(String a, String b) {
+        if (a == null || a.length() == 0) return b;
+        if (b == null || b.length() == 0) return a;
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        StringBuilder sb = new StringBuilder();
+        while ( i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += a.charAt(i--) - '0';
+            }
+            if (j >= 0) {
+                sum += b.charAt(j--) - '0';
+            }
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        if (carry == 1) {
+            sb.append(carry);
+        }
+        return sb.reverse().toString();
+    }
+}
