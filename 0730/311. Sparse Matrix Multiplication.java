c1c2c73d4505dc34be9fc14df12d70e8f6dfdae4
Emily FU <fumengyao5544@gmail.com>
2017-08-20 17:11:31 -0700

    Update 311. Sparse Matrix Multiplication.java

--- 0730/311. Sparse Matrix Multiplication.java
+++ 0730/311. Sparse Matrix Multiplication.java
@@ -1,3 +1,29 @@
+// Given two sparse matrices A and B, return the result of AB.
+
+// You may assume that A's column number is equal to B's row number.
+
+// Example:
+
+// A = [
+//   [ 1, 0, 0],
+//   [-1, 0, 3]
+// ]
+
+// B = [
+//   [ 7, 0, 0 ],
+//   [ 0, 0, 0 ],
+//   [ 0, 0, 1 ]
+// ]
+
+
+//      |  1 0 0 |   | 7 0 0 |   |  7 0 0 |
+// AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
+//                   | 0 0 1 |
+
+
+
+
+
 public class Solution {
     public int[][] multiply(int[][] A, int[][] B) {
         int m = A.length, n = A[0].length, nB = B[0].length;
